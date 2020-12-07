72daae2be1e9e6864d0bf20e8f4c94d01d156a55
shosho <saitama.shohei@gmail.com>
2020-12-07 15:42:36 +0900

    query文字列利用

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 
 class postController extends Controller
 {
-  public function creat($id = 'たこ'){
+  public function creat(Request $request){
     //変数に連想配列、キーがmsgの値を代入
     $data = [
       'msg' => 'bladeの練習、下段には今日の日付が表示',
-      'id' => $id,
+      'id' => $request->id,
     ];
     //viewの第二引数では、配列に指定したキーを変数として渡すことができる
     return view('posts.creat', $data);
