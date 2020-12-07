7dc07470199a28dfff73539a7cdfab13e1c83379
shosho <saitama.shohei@gmail.com>
2020-12-07 15:09:02 +0900

    controllerからbladeへデータ渡し

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class postController extends Controller
 {
   public function creat(){
-    return view('posts.creat');
+    $data = ['msg' => 'bladeの練習、下段には今日の日付が表示' ];
+    return view('posts.creat', $data);
   }
 }
--- resources/views/posts/creat.php
+++ resources/views/posts/creat.php
@@ -2,5 +2,7 @@
   <body>
     <h1>postが表示されるページ</h1>
     <p>sample</p>
+    <p><?php echo $msg; ?></p>
+    <p><?php echo date('Y年n月j日'); ?></p>
   </body>
 </html>
