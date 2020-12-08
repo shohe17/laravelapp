a273c1d52225e86e01d187082bcb22224956eb27
shosho <saitama.shohei@gmail.com>
2020-12-08 11:18:51 +0900

    phpディレクティブ

--- resources/views/posts/creat.blade.php
+++ resources/views/posts/creat.blade.php
@@ -5,8 +5,10 @@
   <h1>continue, brakeの使い方</h1>
   <ol>
     @php
+    <!-- 変数初期化 -->
     $counter = 0;
     @endphp
+    <!-- 繰り返し文 -->
     @while ($counter < count($data))
     <li>{{$data[$counter]}}</li>
     @php
