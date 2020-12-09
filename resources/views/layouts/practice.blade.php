9a713934c4e29c3a08656560667fcc19008e345c
shosho <saitama.shohei@gmail.com>
2020-12-09 13:52:46 +0900

    bladeの表示方法

---
+++ resources/views/layouts/practice.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<!-- セクションの内容を嵌め込んで表示するためのもの -->
+<title>@yield('title')</title>
+</head>
+<body>
+  <h1>デザインはあと、section、yieldの使い方</h1>
+  <!-- セクションの内容を嵌め込んで表示するためのもの -->
+  <h2>@yield('title')</h2>
+  <!-- ページに表示されるコンテンツの区画を定義 -->
+  @section('menubar')
+  <h3 class="menutitle">※メニュー</h3>
+  <ul>
+    <!-- メインのレイアウトでsectionを用意する場合は、endsectionではなくshowを使う -->
+    <li>@show</li>
+  </ul>
+  <div class="content">
+  <!-- 継承先のレイアウトでsection'content'とすればyieldで表示できる -->
+  @yield('content')
+  </div>
+  <div class="footer">
+  <!-- 上と同じ -->
+  @yield('footer')
+  </div>
+</body>
+
+</html>
--- resources/views/posts/creat.blade.php
+++ resources/views/posts/creat.blade.php
@@ -1,27 +1,18 @@
-<html>
-<head>
-<!-- セクションの内容を嵌め込んで表示するためのもの -->
-<title>@yield('title')</title>
-</head>
-<body>
-  <h1>デザインはあと、@section、@yieldの使い方</h1>
-  <!-- セクションの内容を嵌め込んで表示するためのもの -->
-  <h2>@yield('title')</h2>
-  <!-- ページに表示されるコンテンツの区画を定義 -->
-  @section('menubar')
-  <h3 class="menutitle">※メニュー</h3>
-  <ul>
-    <!-- メインのレイアウトでsectionを用意する場合は、endsectionではなくshowを使う -->
-    <li>@show</li>
-  </ul>
-  <div class="content">
-  <!-- 継承先のレイアウトでsection('content')とすれば@yieldで表示できる -->
-  @yield('content')
-  </div>
-  <div class="footer">
-  <!-- 上と同じ -->
-  @yield('footer')
-  </div>
-</body>
+<!-- layoutsフォルダのpracticeというフォルダを読み込み -->
+@extends('layouts.practice')
+<!-- Postという値が表示される -->
+@section('title.Post')
 
-</html>
+<!-- 親レイアウトにmenubarというyieldがあればそこに嵌め込まれる -->
+@section('menubar')
+  @parent
+@endsection
+
+@section('content')
+  <p>ここの文字がpractice.bladeに反映されます</p>
+  <p>必要な記述がdケイル</p>
+@endsection
+
+@section('footer')
+copyright 2020 shosho
+@endsection
