30a9325099f0d0c5be300c3a054939123d697a78
shosho <saitama.shohei@gmail.com>
2020-12-09 15:03:07 +0900

    component使用

--- resources/views/posts/creat.blade.php
+++ resources/views/posts/creat.blade.php
@@ -2,15 +2,24 @@
 @extends('layouts.practice')
 <!-- Postという値が表示される -->
 @section('title.Post')
-
-<!-- 親レイアウトにmenubarというyieldがあればそこに嵌め込まれる -->
-@section('menubar')
-  @parent
 @endsection
 
 @section('content')
   <p>ここの文字がpractice.bladeに反映されます</p>
-  <p>必要な記述がdケイル</p>
+  <p>必要な記述はここ</p>
+
+  <!-- componentsフォルダのmessageファイル読み込み -->
+  @component('components.message')
+    <!-- msgtitle -->
+    <!-- messagebladeの波カッコで指定された変数の読み込み -->
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+    <!-- messageファイルの波カッコで指定されたmsg_contentを読み込み -->
+    @slot('msg_content')
+    これはメッセージの表示です
+    @endslot
+  @endcomponent
 @endsection
 
 @section('footer')
