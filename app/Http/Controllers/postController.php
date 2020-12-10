8c2c12ef25eb123239bcbf0b6d3160445f54eb44
shosho <saitama.shohei@gmail.com>
2020-12-10 12:29:44 +0900

    serviseProvider作成からviewComposer利用

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -7,15 +7,8 @@ use Illuminate\Http\Request;
 class postController extends Controller
 {
   public function creat(){
-    //変数に連想配列を入力=>の左がキーで、右が値
-    $data = [
-      ['name' => 'みき', 'mail' => 'mikimiki@aa'],
-      ['name' => 'みか', 'mail' => 'mikamika@aa'],
-      ['name' => 'みこ', 'mail' => 'mikimiki@aa'],
-
-    ];
     //viewの第二引数では、配列に指定したキーを変数として渡すことができる
-    return view('posts.creat', ['data' => $data]);
+    return view('posts.creat', ['message' => 'Hello!']);
   }
 
   public function post()
