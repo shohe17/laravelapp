5db1f68a581e41004d21a1490eba002fad7c49a2
shosho <saitama.shohei@gmail.com>
2020-12-10 12:30:18 +0900

    serviceProvider作成からviewComposer使用

--- resources/views/posts/creat.blade.php
+++ resources/views/posts/creat.blade.php
@@ -6,26 +6,9 @@
 
 @section('content')
   <p>コンテンツの本文</p>
-  <ul>
-    <!-- 引数1にテンプレート名、２に配列、3に変数名を入れる -->
-    @each('components.item', $data, 'item')
-  </ul>
-  <p>ここの文字がpractice.bladeに反映されます</p>
-  <p>必要な記述はここ</p>
-  @include('components.message', ['msg_title' => 'ok', 'msg_content' => 'サブビュー'])
-
-  <!-- componentsフォルダのmessageファイル読み込み -->
-  @component('components.message')
-    <!-- msgtitle -->
-    <!-- messagebladeの波カッコで指定された変数の読み込み -->
-    @slot('msg_title')
-    CAUTION!
-    @endslot
-    <!-- messageファイルの波カッコで指定されたmsg_contentを読み込み -->
-    @slot('msg_content')
-    これはメッセージの表示です
-    @endslot
-  @endcomponent
+  <p>Controller value<br>'message' = {{ $message }}</p>
+  <p>ViewController value<br>'view_message' = {{ $view_message }}</p>
+  
 @endsection
 
 @section('footer')
