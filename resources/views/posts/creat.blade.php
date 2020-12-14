3e66c71f4f3ce4d5d661635a3b8d1d9b1ed364f2
shosho <saitama.shohei@gmail.com>
2020-12-14 16:06:31 +0900

    validation入力情報の保持

--- resources/views/posts/creat.blade.php
+++ resources/views/posts/creat.blade.php
@@ -10,12 +10,21 @@
   <p>これは<middleware>yahoo.com</middleware>へのリンクです</p>
 
   <p>{{ $msg }}</p>
+  @if(count($errors) > 0)
+  <div>
+    <ul>
+      @foreach($errors->all as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
   <form action="/creat" method="post">
   <table>
     @csrf
-    <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-    <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-    <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+    <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+    <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+    <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
     <tr><th></th><td><input type="submit" value="send"></td></tr>
   </table>
   </form>
