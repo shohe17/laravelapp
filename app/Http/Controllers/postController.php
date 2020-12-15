f308a8b69944121e946bcf5052d42c9686d9238e
shosho <saitama.shohei@gmail.com>
2020-12-15 11:06:29 +0900

    validateの作成

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\CreatRequest;
+use Validator;
 
 class postController extends Controller
 {
@@ -14,9 +14,22 @@ class postController extends Controller
     ]);
   }
 
-  public function postValidation(CreatRequest $request)
+  public function postValidation(Request $request)
   {
+    // makeの1引数は確認する値、2はルールの配列
+    $validator = Validator::make($request->all(), [
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'numeric|between:0,150',
+    ]);
 
+    // failsはvalidation処理に失敗したかどうが検証するメソッド
+    if ($validator->fails()){
+      // エラーじはcreatにリダイレクト
+      // withErrorsは引数にvalidatorインスタンスを渡し、エラーメッセージをリダイレクトに引き継ぐ
+      // withInputは入力情報（送信されたフォームの値）を引き継がせる
+      return redirect('creat')->withErrors($validator)->withInput();
+    }
      return view('posts.creat', [
       'msg' => 'フォームが正しく入力されました！'   
      ]);
